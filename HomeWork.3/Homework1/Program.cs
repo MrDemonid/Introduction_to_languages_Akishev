9c3ab6ad0b25af32080729823730c05dec06c713
MrDemonid <MrDemonid@yandex.ru>
2023-11-24 00:12:00 +0300

    homeworks for Lesson 3

---
+++ HomeWork.3/Homework1/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+    Напишите программу, которая принимает на вход число N и
+выдает произведение чисел от 1 до N.
+Например:
+    4 -> 24
+    5 -> 120
+*/
+
+void Factorial(int num)
+{
+    int sum = 1;
+
+    for (int i = 1; i <= num; i++)
+    {
+        sum *= i;
+    }
+    Console.WriteLine($"Factorial {num} = {sum}");
+}
+
+Console.Write("Enter number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Factorial(n);
---
+++ HomeWork.3/Homework2/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+    Напишите программу, которая принимает на вход число N и
+выдаёт таблицу квадратов чисел от 1 до N.
+Например:
+    5 -> 1, 4, 9, 16, 25
+    2 -> 1, 4
+*/
+
+void PrintSquareTbl(int num)
+{
+    if (num == 0)
+    {
+        Console.WriteLine("Number must be greater than 0");
+        
+    } else {
+        for (int i = 1; i <= num; i++)
+        {
+            Console.Write(i*i);
+            if (i < num)
+                Console.Write(", ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Enter number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+PrintSquareTbl(n);
