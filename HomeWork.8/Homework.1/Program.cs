1b53b52d95120012e9ad29ebd822421d48095daa
MrDemonid <MrDemonid@yandex.ru>
2023-12-08 05:26:35 +0300

    homework 8 - task 1 and 3

---
+++ HomeWork.8/Homework.1/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+    Задайте значения M и N. Напишите программу, которая выведет все 
+натуральные числа в промежутке от M до N. 
+Использовать рекурсию, не использовать циклы.
+Например:
+M=1; N=5; -> 1,2,3,4,5
+M=4; N=8; -> 4,5,6,7,8
+*/
+
+
+/*
+    Ввод корректного натурального числа с консоли.
+*/
+int EnterNaturalNum(string message)
+{
+    int res;
+    
+    while (true)
+    {
+        Console.Write("Enter {0} number: ", message);
+        if (int.TryParse(Console.ReadLine(), out res))
+            if (res > 0)
+                break;
+        Console.WriteLine("The number must be greater than zero!");
+    }
+    return res;
+}
+
+
+/*
+    Собственно задание - вывод натуральных чисел из заданного диапазона
+*/
+void ShowNumbersFromRange(int start, int end)
+{
+    if (end < start)
+        return;
+    ShowNumbersFromRange(start, end-1);     // сначала достигаем начала диапазона
+    if (start != end)
+        Console.Write(", ");
+    Console.Write(end);                     // пошла размотка стека, выводим число
+}
+
+
+Console.Clear();
+int startRange = EnterNaturalNum("start");
+int endRange = EnterNaturalNum("end");
+if (startRange > endRange)
+{
+    int tmp = startRange;
+    startRange = endRange;
+    endRange = tmp;
+}
+
+ShowNumbersFromRange(startRange, endRange);
---
+++ HomeWork.8/Homework.3/Program.cs
@@ -0,0 +1,63 @@
+﻿/*
+    Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+Использовать рекурсию, не использовать циклы.
+[1,2,5,0,10,34] -> 34 10 0 5 2 1
+*/
+
+
+/*
+    Создание массива int[] с заполнением случайными значениями из диапазона [min..max]
+*/
+int[] CreateRandomArray(int size, int min, int max)
+{
+    Random rnd = new Random();
+    int[]array = new int[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max+1);
+    }
+    return array;
+}
+
+/*
+    Вывод на консоль массива
+*/
+void ShowArray(int[] array)
+{
+    for (int idx = 0; idx < array.Length; idx++)
+    {
+        Console.Write(array[idx]);
+        if (idx < array.Length-1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine();
+}
+
+
+/*
+    Вывод элементов массива в обратном порядке
+*/
+void ShowInversedArray(int[] array)
+{
+    if (array.Length == 0)
+        return;
+    ShowInversedArray(array.Skip(1).ToArray());
+    if (array.Length > 1)
+        Console.Write(", ");
+    Console.Write(array[0]);
+}
+
+Console.Write("Enter array size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Enter min: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Enter max: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateRandomArray(size, min, max);
+ShowArray(array);
+
+ShowInversedArray(array);
